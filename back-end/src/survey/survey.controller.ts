1fd17257702fa4e7edee35a05c77d91bc4c82500
hellgamer <19thanks96@gmail.com>
2023-05-21 21:00:27 +0300

    add Authentication according to docs

--- back-end/src/survey/survey.controller.ts
+++ back-end/src/survey/survey.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import {SurveyService} from './survey.service'
+import { Roles } from 'src/auth/roles.decorator';
+import { Role } from 'src/auth/role.enum';
 
 @Controller()
 export class SurveyController {
   constructor(private readonly surveyService: SurveyService ) {}
 
+  @Roles(Role.Admin)
   @Get('survey-result')
   surveyResults(): Array<string> {
     return this.surveyService.surveyResults();
